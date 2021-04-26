5300b64b7d110a490b2c1bdc62a432d052e9be8f
J Loo <junzhongloo@gmail.com>
2021-04-26 12:02:16 +1200

    change color done

--- src/components/quote.js
+++ src/components/quote.js
@@ -9,14 +9,38 @@ class Quote extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            quote: 'initialized text',
-            colorArr: ['red', 'green', 'blue']
+            quote: 'initialized quote',
+            colorArr: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+            '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+            '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+            '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+            '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+            '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+            '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+            '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+            '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+            '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
+            colorNext: '',
+            //colorAfter: '',
         }
+
+        this.selectRandomColor = this.selectRandomColor.bind(this);
     }
 
 
     componentDidMount(){
-        this.getQuote()
+        //get random quote from API and setState
+        this.getQuote();
+
+        //get randomColor from color array
+        let randomColor = this.selectRandomColor()
+        //let randomColorAfter = this.selectRandomColor(randomColorBefore)
+
+        //set next color change
+        this.setState({
+            colorNext:  randomColor,
+            //colorAfter: randomColorAfter
+        })
     }
 
     getQuote(){
@@ -32,17 +56,57 @@ class Quote extends React.Component {
 
     handleClick(){
         this.getQuote()
+        this.changeCSSVarColor()
+        //this.addBackgroundColorAnimation()
+        this.setBackgroundColor()
+        this.changeStateColor()
+        //this.changeColor()
+        //this.addFadeAnimation()
+        //setTimeout(this.setBackgroundColor(), 3000)
+        //setTimeout(this.removeFadeAnimation(),3000)
+
+    }
+
+    changeStateColor(){
         let randomColor = this.selectRandomColor()
-        this.changeColor(randomColor)
+        this.setState({colorNext: randomColor })
     }
 
-    changeColor(color){
-        $('body').css('background-color', color)
-        $('.color-change').css('color', color)
+    changeColor(){
+        //$('#root').css('--varcolorbefore', this.state.colorBefore)
+        //$('#root').css('--varcolorafter', this.state.colorAfter)
+        //$('#text').addClass('color-animation')
     }
 
-    selectRandomColor(){
-        let randomColor = this.state.colorArr[Math.floor(Math.random()*this.state.colorArr.length)];
+    addFadeAnimation(){
+        $('#text').addClass('fade')
+    }
+
+    removeFadeAnimation(){
+        $('#text').removeClass('fade')
+    }
+
+    changeCSSVarColor(){
+        $('#root').css('--varcolor', this.state.colorNext)
+    }
+
+    addBackgroundColorAnimation(){
+        $('#root').addClass('bgcolor-animation')
+    }
+
+    removeBackgroundColorAnimation(){
+        let root = document.getElementById('root')
+        root.classList.remove('color-animation')
+    }
+
+    setBackgroundColor(){
+        $('#root').css('background-color', this.state.colorNext)
+    }
+
+
+    selectRandomColor(colorExcluded){
+        let arrOfColor = [...this.state.colorArr].filter((colorItem) => colorItem !== colorExcluded)
+        let randomColor = arrOfColor[Math.floor(Math.random()*arrOfColor.length)];
         return randomColor
     }
 
@@ -51,16 +115,16 @@ class Quote extends React.Component {
     render(){
         return (
             <div id="quote-box">
-                    <div id="text" className="color-change">
+                    <div id="text" className="">
                         <blockquote><span className="html-quote">&#8220;</span>{this.state.quote}<span className="html-quote">&#8221;</span></blockquote>
-                        <div id="author" className="color-change">- Kanye West</div>
+                        <div id="author" className="">- Kanye West</div>
                     </div>
                     
                     <div className="left-col">
                         <i id="tweet-quote" class="fab fa-twitter-square"></i>
                     </div>
                     <div className="right-col">
-                        <button className="color-change" id="new-quote" onClick={this.handleClick.bind(this)}>New Quote</button>
+                        <button className="" id="new-quote" onClick={this.handleClick.bind(this)}>New Quote</button>
                     </div>
 
             </div>
